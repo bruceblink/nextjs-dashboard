2bda6660d6d8a47e4172748559b2e1cf9f5ad420
likanug <likanug.g@qq.com>
2025-09-12 17:09:49 +0800

    feat: 更新invoices时,表单校验增加错误字段的提示

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -70,13 +70,22 @@ export async function createInvoice(_: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({id: true, date: true});
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const {customerId, amount, status} = UpdateInvoice.parse({
+export async function updateInvoice(id: string, _: State, formData: FormData,) {
+
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+
+    const {customerId, amount, status} = validatedFields.data;
     const amountInCents = amount * 100;
 
     try {
@@ -88,8 +97,7 @@ export async function updateInvoice(id: string, formData: FormData) {
             WHERE id = ${id}
         `;
     } catch (error) {
-        // We'll log the error to the console for now
-        console.error(error);
+        return {message: 'Database Error: Failed to Update Invoice.'};
     }
 
     revalidatePath('/dashboard/invoices');
