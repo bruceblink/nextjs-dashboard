1e037a5c7a0490e93ca17075990a0d6ca032b490
likanug <likanug.g@qq.com>
2025-09-12 13:16:03 +0800

    feat: 实现修改invoice的功能

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,6 +7,8 @@ import {redirect} from "next/navigation";
 
 const sql = postgres(process.env.POSTGRES_URL!);
 
+// 新增 Invoice
+
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string(),
@@ -30,6 +32,31 @@ export async function createInvoice(formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+// 修改 Invoice
+
+const UpdateInvoice = FormSchema.omit({id: true, date: true});
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const {customerId, amount, status} = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId},
+            amount      = ${amountInCents},
+            status      = ${status}
+        WHERE id = ${id}
+    `;
+
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
