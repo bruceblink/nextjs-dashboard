ec47ff4cd8fefdbd857ac985ab46b1197ce2e44f
likanug <likanug.g@qq.com>
2025-09-12 16:32:27 +0800

    chore: 格式化代码

--- app/query/route.ts
+++ app/query/route.ts
@@ -3,18 +3,18 @@ import postgres from 'postgres';
 const sql = postgres(process.env.POSTGRES_URL!);
 
 async function listInvoices() {
-  return sql`
+    return sql`
       SELECT invoices.amount, customers.name
       FROM invoices
                JOIN customers ON invoices.customer_id = customers.id
       WHERE invoices.amount = 666;
   `;
- }
+}
 
 export async function GET() {
-   try {
-   	return Response.json(await listInvoices());
-   } catch (error) {
-   	return Response.json({ error }, { status: 500 });
-  }
+    try {
+        return Response.json(await listInvoices());
+    } catch (error) {
+        return Response.json({error}, {status: 500});
+    }
 }
