03e22e37c06d800d5123f359a67022f509894b33
likanug <likanug.g@qq.com>
2025-09-12 18:19:02 +0800

    feat:  使用NextAuth实现用户登录登出，访问的鉴权和授权等功能

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import {z} from 'zod';
 import postgres from 'postgres';
 import {revalidatePath} from 'next/cache';
 import {redirect} from "next/navigation";
+import {signIn} from "@/auth";
+import {AuthError} from "next-auth";
 
 const sql = postgres(process.env.POSTGRES_URL!);
 
@@ -110,4 +112,23 @@ export async function deleteInvoice(id: string) {
               FROM invoices
               WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+    _: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
